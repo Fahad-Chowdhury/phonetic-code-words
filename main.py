c1484479a9e41578526a01f0d1c3b181e2998f1c
Fahad <fahad@mail.fi>
2024-11-18 16:55:53 +0200

    Include user input validation

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 import os
 import pandas
 
-def code_words():
+
+def generate_phonetic(phonetic_words):
     """ It takes a word as user input and displays phonetic code words
-    for the word. Phonetic code words are generated based on the words
-    listed in the nato_phonetic_alphabet CSV file. """
+    for the word from the 'phonetic_words' dictionary. """
+    user_input = input("Entera word?: ").upper()
+    try:
+        phonetic_code_words = [phonetic_words[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry! Only enter letters in the alphabet.")
+        generate_phonetic(phonetic_words)
+    else:
+        print(f"Phonetic code words:\n{phonetic_code_words}")
+
+
+def code_words():
+    """ It reads phonetic code words for all the letters from 
+    nato_phonetic_alphabet CSV file, and calls generate_phonetic() method to
+    take a word as user input and displays phonetic code words for the word. """
 
     current_dir = os.path.dirname(os.path.abspath(__file__))
     filepath = os.path.join(current_dir, "nato_phonetic_alphabet.csv")
     data = pandas.read_csv(filepath)
     phonetic_words_dict = {row.letter:row.code  for (_, row) in data.iterrows()}
-
-    user_input = input("Entera word?: ").upper()
-    phonetic_code_words = [phonetic_words_dict[letter] for letter in user_input]
-    print(f"Phonetic code words:\n{phonetic_code_words}")
+    generate_phonetic(phonetic_words_dict)
 
 
 if __name__ == "__main__":
